70f8d6c232d9db490e26a0b7b1330c241f7dc6a8
mary <marianapaulina.work@gmail.com>
2024-07-09 20:34:50 -0300

    commit

---
+++ conta/README.md
@@ -0,0 +1,3 @@
+# Projeto ContaBanco
+
+Este projeto é um programa simples em Java que permite criar uma conta bancária inserindo dados via terminal. O programa solicita informações sobre o número da conta, agência, nome do cliente e saldo. Após inserir os dados, ele exibe uma mensagem de confirmação.
---
+++ conta/src/ContaTerminal.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Por favor, digite o número da Conta: ");
+        int numero = scanner.nextInt();
+
+        scanner.nextLine();
+
+        System.out.print("Por favor, digite o número da Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.print("Por favor, digite o nome do Cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.print("Por favor, digite o saldo: ");
+        double saldo = scanner.nextDouble();
+
+        // Mensagem de confirmação
+        String mensagem = "Olá ".concat(nomeCliente).concat(", obrigado por criar uma conta em nosso banco, sua agência é ")
+                                .concat(agencia).concat(", conta ").concat(String.valueOf(numero))
+                                .concat(" e seu saldo ").concat(String.valueOf(saldo)).concat(" já está disponível para saque.");
+        
+        System.out.println(mensagem);
+
+        scanner.close();
+    }
+}
